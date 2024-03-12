1d29dcb946ba8dacbbd5fe5117ca5afbbef54c57
张豆豆 <zhangyuzhou@zhangdoudoudeMacBook-Air.local>
2024-03-11 19:30:42 -0700

    module path fixed

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -77,13 +77,18 @@ resource "aws_lambda_function" "yolov5_image_process_lambda" {
 }
 
 # 创建 Lambda 函数的触发器（例如 S3 触发器）
-resource "aws_lambda_permission" "yolov5_s3_trigger_permission" {
-  statement_id  = "AllowExecutionFromS3"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.yolov5_image_process_lambda.function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = "arn:aws:s3:::${aws_s3_bucket.yolov5_bucket.id}/*"  # 替换为你的 S3 存储桶 ARN
+resource "aws_lambda_function" "yolov5_image_process_lambda" {
+  function_name = "${var.project}-lambda-function"
+  runtime = "python3.8"
+  handler = "handler"  # 设置入口点函数的名称
+  role = aws_iam_role.yolov5_image_process_lambda_execution_role.arn
+  memory_size = 256
+  timeout = 10
+  # 设置 filename 和 source_code_hash 为空字符串
+  filename = ""
+  source_code_hash = ""
 }
+
 
 # 创建 S3 存储桶的事件通知，以触发 Lambda 函数
 resource "aws_s3_bucket_notification" "yolov5_bucket_notification" {
